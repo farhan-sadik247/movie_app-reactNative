c1b97d7877efdfcd610f7b92f56109e6abbe7910
farhan-sadik247 <fsadik2319@gmail.com>
2025-06-02 12:09:26 +0600

    minor changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,9 +3,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-accent">Hello homePage</Text>
-      
+    <View className="flex-1 bg-primary">
+
     </View>
   );
 }
--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      ["babel-preset-expo", { tsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
     plugins: ["react-native-reanimated/plugin"],
   };
 };
+
